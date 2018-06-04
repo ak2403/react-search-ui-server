1315e8680c50663d9a6e913f5a9c99bf1ebd0f2e
AKSpikey <arunpasupathi24@gmail.com>
2018-06-04 22:06:43 +0530

    Origin access header added

--- index.js
+++ index.js
@@ -39,5 +39,13 @@ mongoose.connect('mongodb://akspikey:thats11310104007@ds235850.mlab.com:35850/li
 app.use(bodyParser.json());
 app.use('/repo', listgroup);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Server running'))
